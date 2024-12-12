a8ec95515766cf43dab5904a5d2b892af5289959
Ismael <81421517+IsCa3215@users.noreply.github.com>
2024-12-12 18:37:27 +0100

    Ahora si que si

--- src/screens/FilmScreen.tsx
+++ src/screens/FilmScreen.tsx
@@ -11,11 +11,11 @@ export default function FilmScreen() {
                 data={nowPlaying?.movies}
                 renderItem={({ item }) =>
                     <View>
-                        <Image source={{ uri: "https://image.tmdb.org/t/p/original".concat(item.poster)}} style={{ width: 100, height: 150 }} />
-                        <Text>{item.poster}</Text>
+                        <Image source={{ uri: "https://image.tmdb.org/t/p/original".concat(item.poster)}} style={{ width: 200, height: 250 }} />
+                        <Text style={styles.testo}>{item.title}</Text>
                     </View>
                 }
-                keyExtractor={(item) => item.id.toString()}
+                keyExtractor={(item, index) => `${item.id}_${index}`}
                 onEndReached={() => { sumarPagina(); }} 
                 onEndReachedThreshold={0.5}
             >
@@ -26,4 +26,10 @@ export default function FilmScreen() {
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    testo: {
+        margin: 10,
+        padding: 8,
+        fontSize: 24
+    }
+})
